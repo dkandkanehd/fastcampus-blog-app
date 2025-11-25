9f48ac729bc2dadbe8dccb579df8e19e5e2efac8
msko <41417515+dkandkanehd@users.noreply.github.com>
2025-11-25 09:59:56 +0900

    2025.11.25 2-3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Route, Routes, Navigate } from "react-router-dom";
+import Router from "./components/Router";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<div>Hello World</div>} />
-        <Route path="/about" element={<div>About</div>} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <Router />
     </>
   );
 }
